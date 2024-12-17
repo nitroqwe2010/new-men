6c12e0e12aa182d8e0cc5ff3622df94264bea548
nitroqwe2010 <nitroasd71@gmail.com>
2024-12-17 09:22:03 -0500

    javascrip

---
+++ main.js
@@ -0,0 +1,13 @@
+let ism = prompt('ism kiriting...');
+let yosh = prompt('Yoshinigizni kiriting');
+let manzil = prompt('manzil kiriting');
+let kimsan = prompt('kimligini kiriting');
+let nikname = prompt('nikname ');
+alert('Malumotlarni kiritinganigiz uchun raxmat')
+
+
+console.log("Ism: " ,  ism);
+console.log("Yosh: " ,  yosh);
+console.log("manzil: " ,  manzil);
+console.log("kimsan: " ,  kimsan);
+console.log("nikname: " ,  nikname);
